0e32053cb739d5b88b7da1454b6e07b5642a6577
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2022-10-13 18:27:40 +0530

    import java.util.*; class Main{     public static void main(String[] args){         Scanner n=new Scanner(System.in);         int a=n.nextInt();         double b=32+((a*9)/5.0);         System.out.printf("%.2f",b);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner n=new Scanner(System.in);
+        int a=n.nextInt();
+        double b=32+((a*9)/5.0);
+        System.out.printf("%.2f",b);
+    }
+}
