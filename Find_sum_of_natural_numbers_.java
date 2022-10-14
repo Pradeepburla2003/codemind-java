1aa14fb55b4105076d06d0609a217db1f838b14b
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2022-10-14 07:16:55 +0530

    import java.util.*; class Main{     public static void main(String[] args){         Scanner n=new Scanner(System.in);         int a=n.nextInt();         int d=(a*(a+1))/2;         System.out.printf("%d",d);     } }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner n=new Scanner(System.in);
+        int a=n.nextInt();
+        int d=(a*(a+1))/2;
+        System.out.printf("%d",d);
+    }
+}
